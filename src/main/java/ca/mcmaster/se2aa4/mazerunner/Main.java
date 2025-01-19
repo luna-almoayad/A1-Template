6844262795e10d462085ff603d51cc28fe91b900
Luna <almoayadlu@gmail.com>
2025-01-19 10:59:06 -0500

    Added dependency and logging mechanism

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -5,32 +5,46 @@ import java.io.File;
 import java.io.FileReader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.commons.cli.*; 
 
 public class Main {
 
     private static final Logger logger = LogManager.getLogger();
 
     public static void main(String[] args) {
-        System.out.println("** Starting Maze Runner");
+        logger.info("** Starting Maze Runner");
+
+        //Step 1: use apache CLI to search for -i flag
+        Options options= new Options();
+        options.addOption("i",true, "File path for maze.");
+
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd;
+
         try {
-            System.out.println("**** Reading the maze from file " + args[0]);
-            BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+            cmd = parser.parse(options,args);
+            String fileName = cmd.getOptionValue("i");
+
+            logger.info("**** Reading the maze from file " + fileName);
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
             String line;
+
             while ((line = reader.readLine()) != null) {
                 for (int idx = 0; idx < line.length(); idx++) {
                     if (line.charAt(idx) == '#') {
-                        System.out.print("WALL ");
+                        logger.info("WALL "); //ask if these need to be displayed or if i should use trace 
                     } else if (line.charAt(idx) == ' ') {
-                        System.out.print("PASS ");
+                        logger.info("PASS ");
                     }
                 }
-                System.out.print(System.lineSeparator());
+                System.out.print(System.lineSeparator());// DOES END OF LINE, SO CAN I DO LOGGER.TRACE ("LINE ENDS")
             }
+            reader.close();
         } catch(Exception e) {
-            System.err.println("/!\\ An error has occured /!\\");
+            logger.error("/!\\ An error has occured /!\\");
         }
-        System.out.println("**** Computing path");
-        System.out.println("PATH NOT COMPUTED");
-        System.out.println("** End of MazeRunner");
+        logger.info("**** Computing path");
+        logger.warn("PATH NOT COMPUTED");
+        logger.info("** End of MazeRunner");
     }
 }
