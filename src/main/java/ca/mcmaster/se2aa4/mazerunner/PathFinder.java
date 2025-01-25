888314fd759ca60e32216ba02bc97081335cbef1
Luna <almoayadlu@gmail.com>
2025-01-24 20:29:36 -0500

    Added path for simplest maze

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/PathFinder.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/PathFinder.java
@@ -7,11 +7,16 @@ public class PathFinder {
     public PathFinder(String userPath, Runner runner){
         this.runner = runner;
         this.userPath = userPath;
+        String canon= runner.generatePath();
+        getFactorized(canon);
         checkPath(userPath);
     }
 
-    private void checkPath(String userPath){
+    
+    public void checkPath(String userPath){
+        // convert path in case it is factorized 
         String cleanedPath= expandFactorizedForm(userPath);
+        //determine if it is correct 
         if (cleanedPath.equals(runner.generatePath())){
             System.out.println("Correct Path!");
         } else{
@@ -22,16 +27,20 @@ public class PathFinder {
 
     private String expandFactorizedForm(String path){
         StringBuilder toCanonical = new StringBuilder();
+        // iterate through string until digit is found 
         for (int i=0; i < path.length(); i++){
             char current = path.charAt(i);
             if (Character.isDigit(current)){
                 if (i+1 < path.length()){
+                    //Convert char digit to number and append the corresponding direction
                     char nextChar = path.charAt(i+1);
                     int count= Character.getNumericValue(current);
                     for (int j=0; j<count; j++){
                         toCanonical.append(nextChar);
                     }
+                    //skip next char as it was already processed
                     i++;
+                
                 } else{
                     System.out.println("Cannot have digit with no following letter.");
                 }
@@ -43,4 +52,22 @@ public class PathFinder {
         return toCanonical.toString();
 
     }
+
+    public String getFactorized(String path){
+        StringBuilder toFactorized= new StringBuilder();
+        int count=1;
+        
+        for (int i=0; i<path.length(); i++){
+            if (i < (path.length() -1) && path.charAt(i)== path.charAt(i+1)){
+                count++;
+            } else{
+                toFactorized.append(count).append(path.charAt(i));
+                count=1;
+            }
+        }
+
+        return("The factorized path is: "+ toFactorized.toString());
+    }
 }
+
+
